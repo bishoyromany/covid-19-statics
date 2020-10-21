8d228be6c08c465c587f4010c7b6659e1b2ff737
Bishoy Romany <7bishoyromany7@gmail.com>
2020-10-21 15:03:28 +0200

    Fix Base URL

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import './Sass/App.scss';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./Sass/App.scss";
 
 // pages
-import Home from './Pages/Home'
-import Footer from './Components/Footer'
+import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
 function App() {
   const dev = false;
 
   let config;
-  const base = '/covid-statics';
+  const base = "/";
 
-  if(dev){
+  if (dev) {
     config = {
-      'API' : {
-        'COUNTRIES_TOTAL' : 'http://localhost/covid-statics/src/Caches/countries.json',
-        'GENERAL_CASES' : 'http://localhost/covid-statics/src/Caches/all.json',
-        'HISTORY_CASES' : 'http://localhost/covid-statics/src/Caches/history.json',
-        'HISTORY_CASES_COUNTRY' : 'https://corona.lmao.ninja/v2/historical/',
-      }
+      API: {
+        COUNTRIES_TOTAL:
+          "http://localhost/covid-statics/src/Caches/countries.json",
+        GENERAL_CASES: "http://localhost/covid-statics/src/Caches/all.json",
+        HISTORY_CASES: "http://localhost/covid-statics/src/Caches/history.json",
+        HISTORY_CASES_COUNTRY: "https://corona.lmao.ninja/v2/historical/",
+      },
     };
-  }else{
+  } else {
     config = {
-      'API' : {
-        'COUNTRIES_TOTAL' : 'https://corona.lmao.ninja/v2/countries',
-        'GENERAL_CASES' : 'https://corona.lmao.ninja/v2/all',
-        'HISTORY_CASES' : 'https://corona.lmao.ninja/v2/historical/all?lastdays=30',
-        'HISTORY_CASES_COUNTRY' : 'https://corona.lmao.ninja/v2/historical/',
-      }
+      API: {
+        COUNTRIES_TOTAL: "https://corona.lmao.ninja/v2/countries",
+        GENERAL_CASES: "https://corona.lmao.ninja/v2/all",
+        HISTORY_CASES:
+          "https://corona.lmao.ninja/v2/historical/all?lastdays=30",
+        HISTORY_CASES_COUNTRY: "https://corona.lmao.ninja/v2/historical/",
+      },
     };
   }
-
-
 
   return (
     <Router basename={base}>
